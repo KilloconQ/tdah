d52678c50bda7f463fa258162abae7aa0e7004ed
Killoconq <fjc1997v@gmail.com>
2026-04-06 23:35:23 -0600

    page server

---
+++ src/routes/(auth)/register/+page.server.ts
@@ -0,0 +1,32 @@
+import { fail, redirect } from '@sveltejs/kit';
+
+import { APIError } from 'better-auth/api';
+
+import { auth } from '$lib/server/auth.js';
+
+export const actions = {
+	register: async ({ request }) => {
+		const data = await request.formData();
+		const name = data.get('name') as string;
+		const email = data.get('email') as string;
+		const password = data.get('password') as string;
+
+		try {
+			await auth.api.signUpEmail({
+				body: {
+					email,
+					password,
+					name,
+					callbackURL: '/dashboard'
+				}
+			});
+		} catch (error) {
+			if (error instanceof APIError) {
+				return fail(400, { message: error.message });
+			}
+			return fail(500, { message: 'Unexpected error' });
+		}
+
+		return redirect(302, '/demo/better-auth');
+	}
+};
